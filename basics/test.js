43c5d67ee00dd066a66e1c7b08c0e35e1faa8667
Shubham <s.jangid1100@gmail.com>
2023-08-20 17:22:17 +0530

    push1

--- basics/test.js
+++ basics/test.js
@@ -1,3 +1,10 @@
-console.log(
-"shubham"
-);
+console.log("shubham")    //basic output
+const Name = "Shubham"
+let surname ="Surname"
+var age = "age"
+city ="city"
+//Name = "Name"   //constant >> once defined cant be changed
+surname = "Jangid"
+age = 21   // string to integer
+let state ;
+console.table([Name,surname,age,city,state])
