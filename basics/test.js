6571b3ee0352b2252aad193d050bd16dd43a9a6a
Shubham <s.jangid1100@gmail.com>
2023-08-20 17:32:13 +0530

    a

--- basics/test.js
+++ basics/test.js
@@ -5,6 +5,10 @@ var age = "age"
 city ="city"
 //Name = "Name"   //constant >> once defined cant be changed
 surname = "Jangid"
-age = 21   // string to integer
+age = 21   // data type changed from string to integer
 let state ;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
 console.table([Name,surname,age,city,state])
