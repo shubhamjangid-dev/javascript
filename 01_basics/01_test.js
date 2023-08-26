d96aece6ae6107f8bc5d390da46aa9e65e1d1cce
shubhamjangid-dev <142687562+shubhamjangid-dev@users.noreply.github.com>
2023-08-26 13:38:35 +0000

    rename

---
+++ 01_basics/01_test.js
@@ -0,0 +1,14 @@
+console.log("shubham")    //basic output
+const Name = "Shubham"
+let surname ="Surname"
+var age = "age"
+city ="city"
+//Name = "Name"   //constant >> once defined cant be changed
+surname = "Jangid"
+age = 21   // data type changed from string to integer
+let state ;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
+console.table([Name,surname,age,city,state])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//"use strict"  //treat all js code as newer version
+//alert(3+3)  //we are using node js, not browser
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//undefined => not defined
+//null => empty/standalone value
+//symbol => unique
+
+//object 
+console.log(typeof "shubham");  // string
+console.log(typeof false);      // boolean
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
