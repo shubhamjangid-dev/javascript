5a80c6e5ef7b451d8e3fba61b7ca764a83a9f538
shubhamjangid-dev <142687562+shubhamjangid-dev@users.noreply.github.com>
2023-08-27 10:58:11 +0000

    01

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,35 @@
 let arr = [0, 1, 2, 3, 4, 5,"Shubham"]
 
 // console.log(arr.length);
-console.log(arr.toString());
+// console.log(arr.toString());
+
+
+// console.log("A" ,arr);
+// arr.push("Jangir")
+// arr.push("Rajasthan")
+// arr.pop()
+// console.log("B" ,arr);
+
+
+// console.log("A",arr);    // A [ 0, 1, 2, 3, 4, 5, 'Shubham' ]
+// arr.unshift(-1);
+// arr.unshift(99)
+// console.log("B",arr);    // B [ 99, -1, 0, 1, 2, 3, 4, 5, 'Shubham' ]
+// arr.shift()
+// console.log("C",arr);    // C [ -1, 0, 1, 2, 3, 4, 5, 'Shubham' ]
+
+
+// console.log(arr.includes(1));
+// console.log(arr.indexOf("Shubham"));
+
+
+//Slice ,Splice
+
+// console.log("A",arr);               // A [ 0, 1, 2, 3, 4, 5, 'Shubham' ]
+// console.log("B",arr.slice(1,3));    // B [ 1, 2 ] 
+
+// console.log("C",arr);               // C [ 0, 1, 2, 3, 4, 5, 'Shubham' ]
+
+// console.log("D",arr.splice(1,3));   // D [ 1, 2, 3 ]
+// console.log("E",arr);               // E [ 0, 4, 5, 'Shubham' ]
+
